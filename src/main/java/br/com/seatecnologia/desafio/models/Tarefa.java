5dd8d287d2eca3b3376b732c369a58cd7d971389
theAapache <arthurnvsm@gmail.com>
2020-09-02 13:57:05 -0300

    atualizações em entidades e controllers

--- src/main/java/br/com/seatecnologia/desafio/models/Tarefa.java
+++ src/main/java/br/com/seatecnologia/desafio/models/Tarefa.java
@@ -11,4 +11,46 @@ public class Tarefa {
 	
 	private Funcionario funcionario;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Boolean getEstaConcluida() {
+		return estaConcluida;
+	}
+
+	public void setEstaConcluida(Boolean estaConcluida) {
+		this.estaConcluida = estaConcluida;
+	}
+
+	public Integer getIndex() {
+		return index;
+	}
+
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+	
+
 }
